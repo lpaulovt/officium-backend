e02b5a236b626810ba1351744676ce52316649d3
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-04-03 20:25:02 -0300

    chore: Add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
-
-app.listen(3333, () => console.log("Server is running"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(router);
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World!!!",
-  });
-});
+app.listen(3333, () => console.log("Server is running"));
