e8c0cb6bbe1112a03fda9fb6074c906864a7a840
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-04-27 21:58:54 -0300

    chore: creating update avatar user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const createUsersController = new CreateUserController();
+const updateUserAvatarControler = new UpdateUserAvatarController();
+
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 usersRoutes.post("/", createUsersController.handle);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarControler.handle
+);
 
 export { usersRoutes };
