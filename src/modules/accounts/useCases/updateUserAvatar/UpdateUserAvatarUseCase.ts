4b646c12a1d5119adf43c87be7618385b7b0b9de
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-04-27 22:10:17 -0300

    chore: remove duplicate avatar files

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUserRepository } from "../../repositories/IUsersRepository";
+
 
 interface IRequest {
   user_id: string;
@@ -17,6 +19,9 @@ class UpdateUserAvatarUseCase {
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_file;
 
     await this.usersRepository.create(user);
