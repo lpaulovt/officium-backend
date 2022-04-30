54b1aad06e7823a561bc2cef44556825195ad637
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-04-29 22:50:12 -0300

    chore: creating is admin middleware

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const createUsersController = new CreateCarController();
 
 const carsRoutes = Router();
 
-carsRoutes.post("/", createUsersController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createUsersController.handle
+);
 
 export { carsRoutes };
