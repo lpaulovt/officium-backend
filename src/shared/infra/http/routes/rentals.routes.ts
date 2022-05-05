8dc7006a0b587a668b7815fcc11baa945ae890d8
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-05-05 18:17:59 -0300

    chore: list rentals use case

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalByUser/ListRentalsByUserController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 const rentalsRoutes = Router();
 
@@ -23,6 +25,13 @@ rentalsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   devolutionRentalController.handle
+);
+
+rentalsRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  ensureAdmin,
+  listRentalsByUserController.handle
 );
 
 export { rentalsRoutes };
