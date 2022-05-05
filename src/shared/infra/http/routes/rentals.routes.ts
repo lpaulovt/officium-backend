b372b10713ddc61101dfc6757e51b27ab5722e99
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-05-05 18:05:41 -0300

    chore: devolution use case

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
+
 const rentalsRoutes = Router();
 
 rentalsRoutes.post(
@@ -13,6 +16,13 @@ rentalsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createRentalController.handle
+);
+
+rentalsRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  devolutionRentalController.handle
 );
 
 export { rentalsRoutes };
