2c0c85861dc76992555f27c78023de0a21530edb
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-05-05 19:56:07 -0300

    fix: refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "aa33256bbdaea0f5adbfae7f45249c03",
-  expires_in_token: "15m",
+  expires_in_token: "60m",
   secret_refresh_token: "125c41faf66e2c351bff02def3b22cab",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
