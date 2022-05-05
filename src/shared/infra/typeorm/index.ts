557ad960488d76706d339d1d1cb65d0537820077
Paulo Lopes <silvavitorlopes711@gmail.com>
2022-05-05 17:46:51 -0300

    fix: car available status

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,11 +5,7 @@ export default async (host = "database_ignite"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
-      database:
-        process.env.NODE_ENV === "test"
-          ? "rentx_test"
-          : defaultOptions.database,
+      host,
     })
   );
 };
